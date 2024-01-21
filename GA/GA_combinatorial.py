b3c60c687495528ed505018b30755744de73fe6a
zartwilly <zartwilly@gmail.com>
2024-01-21 12:27:41 +0100

    GA_combi: define the constances of GA.

--- GA/GA_combinatorial.py
+++ GA/GA_combinatorial.py
@@ -14,7 +14,11 @@ import numpy as np
 #                   Constances : start
 #
 ###############################################################################
-
+P_C = 1             # Probability of Crossover
+P_M = 0.3           # Probability of Mutation
+K = 3               # Tournament Selection
+POPULATION = 100    # Population per generation
+GENERATION = 30     # Number of Generation
 ###############################################################################
 # 
 #                   Constances : ennd
